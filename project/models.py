44fbb0488e7929319a161731f27f1f06df9ba212
Emile Hanau <hanau.emile@gmail.com>
2021-09-06 16:00:59 +0200

    initial push to repo -> basic user login management

---
+++ project/models.py
@@ -0,0 +1,16 @@
+from flask_login import UserMixin
+from . import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+class ProjectIdea(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(1000))
+    description = db.Column(db.String(100000))
+    img = db.Column(db.String(1000))
